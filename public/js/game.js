29ea3c9d59da1178a02c90fc1314b6934eb2ee72
David Tsu <davidtsu97@gmail.com>
2020-05-12 14:33:27 -0700

    game page works, resized images, css

--- public/js/game.js
+++ public/js/game.js
@@ -1,191 +1,128 @@
-// (async function () {
-//     const db = await dbConnection();
-// 	await db.dropDatabase();
-//     const all_dicts = dictionaries.getAllDictionaries();
-// 	await db.serverConfig.close();
-    
-//     let themeElement = document.getElementById("game-theme");
-//     for(let item in all_dicts){
-//         console.log('all_dicts=', all_dicts)
-//         var dict = document.createElement('option')
-//         var dict_name = document.createTextNode(item['theme'])
-//         dict.appendChild(dict_name)
-//         themeElement.appendChild(dict)
-//     }
-// })();
+(function () {
+    const answer = 'filler'.toUpperCase();          // TODO: REPLACE THIS WITH INPUT WORD
+    let guessed = [];
+    let incorrect = [];
+    let wordStatus = null;
+    let chancesLeft = 6;
+    updateDisplay(answer);
 
-var beachArray = ['sand', 'beach', 'surfing', 'waves', 'boardwalk', 'surfboard', 'shore', 'coast', 'sandbar'];
-var campingArray = ['backpack', 'fishing' ,'campfire', 'forest', 'birds', 'animals', 'trees', 'waterfall'];
-
-let selectThemeForm = document.getElementById("selectThemeForm");
-let themeElement = document.getElementById("theme");
-let answer = '';
-let guessed = [];
-let wordStatus = null;
-
-let chancesLeft = 6
-function beach(){
-    rand = Math.floor(Math.random()*beachArray.length);
-    answer = beachArray[rand].toUpperCase();
-}
-
-function camping(){
-    rand = Math.floor(Math.random()*campingArray.length);
-    answer = campingArray[rand].toUpperCase();
-}
-
-selectThemeForm.addEventListener("submit", (event) =>{
-    event.preventDefault();
-    if (themeElement.value === 'camping'){
-        camping();
-        gameplay();
-        
-    } else if (themeElement.value === 'beach'){
-        beach();
-        gameplay();
-}
-});
-
-
-function generateButtons(){
-    let buttonsHTML = 
-    `<button id="a" onclick="handleGuess('a')">a</button>
-    <button id="b" onclick="handleGuess('b')">b</button>
-    <button id="c" onclick="handleGuess('c')">c</button>
-    <button id="d" onclick="handleGuess('d')">d</button>
-    <button id="e" onclick="handleGuess('e')">e</button>
-    <button id="f" onclick="handleGuess('f')">f</button>
-    <button id="g" onclick="handleGuess('g')">g</button>
-    <button id="h" onclick="handleGuess('h')">h</button>
-    <button id="i" onclick="handleGuess('i')">i</button>
-    <button id="j" onclick="handleGuess('j')">j</button>
-    <button id="k" onclick="handleGuess('k')">k</button>
-    <button id="l" onclick="handleGuess('l')">l</button>
-    <button id="m" onclick="handleGuess('m')">m</button>
-    <button id="n" onclick="handleGuess('n')">n</button><br>
-    <button id="o" onclick="handleGuess('o')">o</button>
-    <button id="p" onclick="handleGuess('p')">p</button>
-    <button id="q" onclick="handleGuess('q')">q</button>
-    <button id="r" onclick="handleGuess('r')">r</button>
-    <button id="s" onclick="handleGuess('s')">s</button>
-    <button id="t" onclick="handleGuess('t')">t</button>
-    <button id="u" onclick="handleGuess('u')">u</button>
-    <button id="v" onclick="handleGuess('v')">v</button>
-    <button id="w" onclick="handleGuess('w')">w</button>
-    <button id="x" onclick="handleGuess('x')">x</button>
-    <button id="y" onclick="handleGuess('y')">y</button>
-    <button id="z" onclick="handleGuess('z')">z</button>`
-
-    document.getElementById('keyboard').innerHTML = buttonsHTML;
-}
-
-// ensures that text input field only takes one capital-letter character.
-function validate(input){
-    input.value = input.value.replace(/\W|\d/g, '').substr(0, 1).toUpperCase();
-}
-
-// something here isn't working. It used to take in an input parameter, but I can't get anything to work.
-function handleGuess() {
-    chosenLetter = 'A';//document.getElementById('atext').value;
-    if (!chosenLetter) console.log('No input!', chosenLetter);
-    try {
-        let patt=/[A-z]/g;
-        if (patt.test(chosenLetter)) {
-            guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
-            //document.getElementById(chosenLetter).setAttribute('disabled', true);
-        
-            if (answer.indexOf(chosenLetter) >= 0) {
-                guessedWord();
-                checkResult();
-            } else if (answer.indexOf(chosenLetter) === -1) {
-                chancesLeft = chancesLeft - 1;
-                updateChance();
-                checkResult();
+    const hangman_methods = {
+        handleGuess(char) {
+            if (!char) throw 'No input.';
+            try {
+                let patt=/[A-z]/g;
+                if (patt.test(char)) {
+                    if (guessed.indexOf(char) === -1) {
+                        guessed.push(char)
+                    } else {
+                        throw 'Letter has already been guessed.'
+                    }
+                
+                    if (answer.indexOf(char) >= 0) {
+                        this.guessedWord();
+                        this.checkResult();
+                    } else if (answer.indexOf(char) === -1) {
+                        chancesLeft = chancesLeft - 1;
+                        this.addIncorrect(char);
+                        this.cycleImage();
+                        this.checkResult();
+                    }
+                }
+            } catch (err) {
+                throw(err);
+            }
+        },
+        guessedWord() {
+            wordStatus = []
+            alphabets = answer.toUpperCase().split('')
+            for(i = 0; i < alphabets.length; i++ ){
+                if (guessed.includes(alphabets[i])){
+                    wordStatus.push(alphabets[i])
+                } else {
+                    wordStatus.push(' _ ')
+                }
+            }
+            wordStatus = wordStatus.join('')
+            document.getElementById('wordStatus').innerHTML = wordStatus;
+        },
+        addIncorrect(char) {
+            incorrect.push(char);
+            document.getElementById('incorrect-guesses').innerHTML = incorrect;
+        },
+        cycleImage() {
+            const img = document.getElementById('chances-left')
+            switch (chancesLeft){
+                default:
+                    img.src='/public/img/6_left.png';
+                    break;
+                case 6:
+                    img.src='/public/img/6_left.png';
+                    break;
+                case 5:
+                    img.src='/public/img/5_left.png';
+                    break;
+                case 4:
+                    img.src='/public/img/4_left.png';
+                    break;
+                case 3:
+                    img.src='/public/img/3_left.png';
+                    break;
+                case 2:
+                    img.src='/public/img/2_left.png';
+                    break;
+                case 1:
+                    img.src='/public/img/1_left.png';
+                    break;
+                case 0:
+                    img.src='/public/img/0_left.png';
+            }
+        },
+        checkResult() {
+            if (wordStatus === answer) {
+                document.getElementById('game-results').classList.add('success');
+                document.getElementById('game-results').innerHTML = 'You Won!!!';
+                // TODO: send data to DB
+            }
+            if (chancesLeft == 0) {
+                document.getElementById('wordStatus').innerHTML = 'The answer was: ' + answer;
+                document.getElementById('game-results').classList.add('failure');
+                document.getElementById('game-results').innerHTML = 'You Lost!!!';
+                // TODO: send data to DB
             }
         }
-    } catch (err) {
-        console.log(err);
+    };
+
+    const gameForm = document.getElementById('game-form');
+
+    if(gameForm) {
+        const errorContainer = document.getElementById("error-container");
+        const errorTextElement = errorContainer.getElementsByClassName('text-goes-here')[0];
+
+        gameForm.addEventListener("submit", event => {
+            event.preventDefault();         
+
+            try {
+                errorContainer.hidden = true;
+
+                const input_char = document.getElementById('atext').value;
+                hangman_methods.handleGuess(input_char);
+
+            } catch (e) {
+                const message = typeof e === "string" ? e : e.message;
+                errorTextElement.textContent = e;
+                errorContainer.hidden = false;
+            }
+        })
     }
-}
+})();
 
-function guessedWord(){
-    wordStatus = []
-    alphabets = answer.toUpperCase().split('')
-    for(i = 0; i < alphabets.length; i++ ){
-        if (guessed.includes(alphabets[i])){
-            wordStatus.push(alphabets[i])
-        }else{
-            wordStatus.push(' _ ')
-        }
-    }
-    wordStatus = wordStatus.join('')
-    document.getElementById('playgame').innerHTML = 'Guess the below word';
-    document.getElementById('wordStatus').innerHTML = wordStatus;
-}
+// ensures that text input field only takes one capital-letter character.
+function validate(input) {
+    input.value = input.value.replace(/\W|\d/g, '').substr(0, 1).toUpperCase();
+};
 
-
-function checkResult() {
-    if (wordStatus === answer) {
-    document.getElementById('keyboard').innerHTML = 'You Won!!!';
-    }
-    if (chancesLeft == 0) {
-        document.getElementById('wordStatus').innerHTML = 'The answer was: ' + answer;
-        document.getElementById('keyboard').innerHTML = 'You Lost!!!';
-    }
-  
-}
-
-// currently used to update chancesLeft and cycle the image.
-function updateChance(){
-    document.getElementById('chances').innerHTML = "Chances Left: " +chancesLeft
-    const img = document.getElementById('chances-left')
-
-    switch (chancesLeft){
-        default:
-            img.src='/public/img/6_left.png';
-            break;
-        case 6:
-            img.src='/public/img/6_left.png';
-            break;
-        case 5:
-            img.src='/public/img/5_left.png';
-            break;
-        case 4:
-            img.src='/public/img/4_left.png';
-            break;
-        case 3:
-            img.src='/public/img/3_left.png';
-            break;
-        case 2:
-            img.src='/public/img/2_left.png';
-            break;
-        case 1:
-            img.src='/public/img/1_left.png';
-            break;
-        case 0:
-            img.src='/public/img/0_left.png';
-    }
-}
-  
-function reset() {
-    event.preventDefault();
-    if (themeElement.value === 'camping'){
-        camping();
-        gameplay();
-        
-    } else if (themeElement.value === 'beach'){
-        beach();
-        gameplay();
-    }
-}
-
-document.getElementById('chances').innerHTML = "Chances Left: " +chancesLeft;
-
-function gameplay(){
-    chancesLeft = 6;
-    guessed = [];
-
-    updateChance();
-    guessedWord();
-    generateButtons();
-}
+// ensures that word guesses are always displayed, etc.
+function updateDisplay(answer) {
+    document.getElementById('wordStatus').innerHTML = ' _ '.repeat(answer.length);
+    document.getElementById("error-container").hidden = true;
+};
